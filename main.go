0b8297426996a55c59c3430a937d70d405e2b298
Archit Kumar <architxkumar@proton.me>
2025-06-25 16:07:08 +0530

    chore: add basic argument parsing for "add" command

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	app := cli.NewApp()
+	app.Name = "Expense-Tracker"
+	app.Usage = "A simple expense tracker to manage your finances."
+	app.Commands = []cli.Command{
+		{Name: "add", Usage: "Add an expense with a description and amount", Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:     "description, d",
+				Usage:    "Description of the expense",
+				Required: true,
+			},
+		}, Action: func(c *cli.Context) error {
+			description := c.String("description")
+			fmt.Printf("Adding expense to %s\n", description)
+			return nil
+		},
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
